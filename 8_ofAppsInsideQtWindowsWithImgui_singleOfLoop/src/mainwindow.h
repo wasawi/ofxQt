89c71ad8074c3b91711ec3d3f416a62182967ff5
jordiexvision <jordi@exvision.co.jp>
2017-06-21 15:17:20 +0900

    added single ofLoop example

---
+++ 8_ofAppsInsideQtWindowsWithImgui_singleOfLoop/src/mainwindow.h
@@ -0,0 +1,44 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include "ofMain.h"
+#include "ui_mainwindow.h"
+#include "device.h"
+
+// include Qt always at the end or face glew error
+#include <vector>
+#include <QMainWindow>
+#include <QMdiSubWindow>
+#include <QWidget>
+
+namespace Ui {
+class MainWindow;
+}
+
+class ofApp;
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+	shared_ptr<ofMainLoop> loop;
+	QTimer *timer;
+
+public slots:
+
+void on_actionAdd_OF_app_triggered() {
+	loadSubWindow(new device(this));
+}
+
+private:
+	void newframe();
+	Ui::MainWindow *ui;
+	void loadSubWindow(QWidget* widget);
+
+};
+
+#endif // MAINWINDOW_H
---
+++ 8_ofAppsInsideQtWindowsWithImgui_singleOfLoop/src/ofApp.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+#include "ofxImGui.h"
+
+// include Qt always at the end or face glew error
+#include <QSettings>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class ofApp : public ofBaseApp{
+public:
+	ofApp();
+	~ofApp();
+
+	shared_ptr<ofAppBaseWindow> window;
+
+        void setup();
+        void update();
+        void draw();
+
+        void keyPressed(ofKeyEventArgs& key);
+        void keyReleased(ofKeyEventArgs& key);
+        void mouseMoved(int x, int y);
+        void mouseDragged(int x, int y, int button);
+        void mousePressed(int x, int y, int button);
+        void mouseReleased(int x, int y, int button);
+        void mouseEntered(int x, int y);
+        void mouseExited(int x, int y);
+        void windowResized(int w, int h);
+        void dragEvent(ofDragInfo dragInfo);
+        void gotMessage(ofMessage msg);
+		
+		ofPoint cursor;
+		string key_str;
+
+		ofParameterGroup parameters;
+		ofParameter<float> radius;
+		ofParameter<ofColor> color;
+		ofParameter<float> framerate;
+		ofParameter<bool> verticalSync;
+//		ofxPanel OfGUI;
+
+
+		ofxImGui::Gui imgui;
+
+
+		void setFramerate(float& value);
+		void setVerticalSync(bool& value);
+};
