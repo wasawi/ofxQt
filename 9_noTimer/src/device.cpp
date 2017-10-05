3dfb934ae11b9749d2b64c0a3f6d5282ea1247f3
jordiexvision <jordi@exvision.co.jp>
2017-10-05 18:48:03 +0900

    ofRunApp must be after creating setLayout

--- 9_noTimer/src/device.cpp
+++ 9_noTimer/src/device.cpp
@@ -9,26 +9,30 @@ device::device( QWidget *parent)//shared_ptr<ofApp> _ofAppPtr,
 	// here we create a ofapp with an ofQtWindow
 	// then we extract a pointer to QOpenGLWidget
 	// and we insert it in the layout.
-	ofSetLogLevel(OF_LOG_VERBOSE);
-	ofLogToConsole();
+//	ofSetLogLevel(OF_LOG_VERBOSE);
+//	ofLogToConsole();
 
 	// create an app and a window and initialize
 	// ofLoop is not used, Qt will call events
 	ofAppPtr = make_shared<ofApp>();
 	windowPtr = make_shared<ofAppQtWindow>(this->parentWidget());
+	windowPtr->setAppPtr(ofAppPtr);
 	ofAppPtr->window = windowPtr;
 	ofSetupOpenGL(windowPtr, 400, 400, OF_WINDOW);
-	ofRunApp(windowPtr, ofAppPtr);
-
+	
 	// add widget to layout
 	layout = ui.horizontalLayout;
 	layout->addWidget(windowPtr->getQOpenGLWidget());
 	ui.widget_2->setLayout(layout);
+
+	// initialize OF must be here!
+	ofRunApp(windowPtr, ofAppPtr);
 }
 
 device::~device()
 {
 	// tell the ofMainLoop that this window has to close.
+	ofAppPtr = nullptr; // this removes warnings on console
 	ofGetMainLoop()->removeWindow(windowPtr);
 }
 
